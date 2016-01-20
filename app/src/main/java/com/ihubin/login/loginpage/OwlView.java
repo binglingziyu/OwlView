0acf8b0d7450447ee0d1e5da3f5e688a99396b78
hubin <7629654@qq.com>
2016-01-20 15:07:04 +0800

    OwlView extends View , 动画方式和效果图有些差距,就不改了  😄^_^

--- app/src/main/java/com/ihubin/login/loginpage/OwlView.java
+++ app/src/main/java/com/ihubin/login/loginpage/OwlView.java
@@ -11,20 +11,23 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.animation.LinearInterpolator;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 /**
- * 这个改成View比较好,有时间改改
+ * 猫头鹰输入密码的时候捂眼
+ * 布局文件中宽=175dp,高=107dp,也可以不写默认,只是为了预览
  */
-public class OwlView extends FrameLayout{
+public class OwlView extends View {
 
     private Context mContext;
-    private ImageView iv_owl;
+    //private ImageView iv_owl;
+
+    private Bitmap bm_owl;
 
     private Bitmap bm_owl_arm_left;
 
@@ -56,20 +59,28 @@ public class OwlView extends FrameLayout{
 
     private void init(){
         setBackgroundColor(Color.TRANSPARENT);
-        iv_owl = new ImageView(mContext);
-        iv_owl.setImageResource(R.mipmap.owl_login);
-        LayoutParams iv_owl_lp = new LayoutParams(dip2px(115),dip2px(107));
 
-        iv_owl_lp.setMargins(dip2px(30),0,dip2px(30),0);
-        addView(iv_owl,iv_owl_lp);
+        getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                ViewGroup.LayoutParams lp = getLayoutParams();
+                if(lp==null)return;
+                lp.width = dip2px(175);
+                lp.height = dip2px(107);
+                setLayoutParams(lp);
+                setTranslationY(dip2px(9));
+            }
+        });
 
+
+        bm_owl = BitmapFactory.decodeResource(getResources(),R.mipmap.owl_login);
         bm_owl_arm_left = BitmapFactory.decodeResource(getResources(),R.mipmap.owl_login_arm_left);
         bm_owl_arm_right = BitmapFactory.decodeResource(getResources(),R.mipmap.owl_login_arm_right);
 
+        bm_owl = compressBitmap(bm_owl,dip2px(115),dip2px(107),false);
         bm_owl_arm_left = compressBitmap(bm_owl_arm_left,dip2px(40),dip2px(65),true);
         bm_owl_arm_right = compressBitmap(bm_owl_arm_right,dip2px(40),dip2px(65),true);
-        //bm_owl_arm_left = BitmapUtils.compressBitmap(getResources(),R.mipmap.owl_login_arm_left,dip2px(30),dip2px(50));
-        //bm_owl_arm_right = BitmapUtils.compressBitmap(getResources(),R.mipmap.owl_login_arm_right,dip2px(30),dip2px(50));
 
         bm_height = bm_owl_arm_left.getHeight()/3*2-dip2px(10);
 
@@ -153,15 +164,32 @@ public class OwlView extends FrameLayout{
     }
 
     @Override
-    protected void dispatchDraw(Canvas canvas) {
-        super.dispatchDraw(canvas);
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(bm_owl,new Rect(0,0,bm_owl.getWidth(),bm_owl.getHeight()),new Rect(dip2px(30),0,dip2px(30)+bm_owl.getWidth(),bm_owl.getHeight()),handPaintAfter);
+
         handPaintBefore.setAlpha(alpha);
         canvas.drawOval(new RectF(move_length,getHeight()-dip2px(20),move_length+dip2px(30),getHeight()),handPaintBefore);
         canvas.drawOval(new RectF(getWidth()-dip2px(30)-move_length,getHeight()-dip2px(20),getWidth()-move_length,getHeight()),handPaintBefore);
 
         canvas.drawBitmap(bm_owl_arm_left,new Rect(0,0,bm_owl_arm_left.getWidth(), moveHeight),new Rect(dip2px(43),getHeight()-moveHeight-dip2px(10),dip2px(43)+bm_owl_arm_left.getWidth(),getHeight()-dip2px(9)),handPaintAfter);
         canvas.drawBitmap(bm_owl_arm_right,new Rect(0,0,bm_owl_arm_right.getWidth(), moveHeight),new Rect(getWidth()-dip2px(40)-bm_owl_arm_right.getWidth(),getHeight()-moveHeight-dip2px(10),getWidth()-dip2px(40),getHeight()-dip2px(9)),handPaintAfter);
+
     }
+
+//    @Override
+//    protected void dispatchDraw(Canvas canvas) {
+//        super.dispatchDraw(canvas);
+//
+//        canvas.drawBitmap(bm_owl,new Rect(0,0,bm_owl.getWidth(),bm_owl.getHeight()),new Rect(dip2px(30),0,dip2px(30)+bm_owl.getWidth(),bm_owl.getHeight()),handPaintAfter);
+//
+//        handPaintBefore.setAlpha(alpha);
+//        canvas.drawOval(new RectF(move_length,getHeight()-dip2px(20),move_length+dip2px(30),getHeight()),handPaintBefore);
+//        canvas.drawOval(new RectF(getWidth()-dip2px(30)-move_length,getHeight()-dip2px(20),getWidth()-move_length,getHeight()),handPaintBefore);
+//
+//        canvas.drawBitmap(bm_owl_arm_left,new Rect(0,0,bm_owl_arm_left.getWidth(), moveHeight),new Rect(dip2px(43),getHeight()-moveHeight-dip2px(10),dip2px(43)+bm_owl_arm_left.getWidth(),getHeight()-dip2px(9)),handPaintAfter);
+//        canvas.drawBitmap(bm_owl_arm_right,new Rect(0,0,bm_owl_arm_right.getWidth(), moveHeight),new Rect(getWidth()-dip2px(40)-bm_owl_arm_right.getWidth(),getHeight()-moveHeight-dip2px(10),getWidth()-dip2px(40),getHeight()-dip2px(9)),handPaintAfter);
+//    }
 
     private int dip2px(float dpValue) {
         final float scale = mContext.getResources().getDisplayMetrics().density;
